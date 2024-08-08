d6276f8dc38e0add69eb850ec63e2fa23f83c895
julia-is-awake <juliasorgetzalves@gmail.com>
2024-08-07 22:57:53 -0300

    pagina de chats finalizada!

--- src/pages/ChatsBrowser/index.jsx
+++ src/pages/ChatsBrowser/index.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import "./chatsBrowser.css";
+import { Link } from "react-router-dom";
 
 import mediumGlobe from "../../../public/mediumGlobe.png";
+import tabOption from "../../../public/tabOption.svg";
+import privateRoom from "../../../public/privateRoom.png";
+import publicRoom from "../../../public/publicRoom.png";
 
 function ChatsBrowser() {
   const navigate = useNavigate();
@@ -39,13 +43,50 @@ function ChatsBrowser() {
     <div className="container">
       <div className="browser-window">
         <div className="browser-header">
-          <img src={mediumGlobe} alt="icone de globo" />
+          <div className="header-title">
+            <img src={mediumGlobe} alt="icone de globo" />
+            <h1>Chat-in.ho</h1>
+          </div>
+          <div className="tab-options">
+            <button>
+              <p
+                style={{
+                  color: "#818181",
+                  textShadow: "rgba(0,0,0,0.25) 0 3px 1px",
+                }}
+              >
+                _
+              </p>
+            </button>
+            <button>
+              <img
+                src={tabOption}
+                alt="icone do header"
+                style={{ filter: "drop-shadow(0 3px 1px rgba(0,0,0,0.25)" }}
+              />
+            </button>
+            <button
+              style={{
+                cursor: "pointer",
+                textShadow: "rgba(0,0,0,0.25) 0 3px 1px",
+              }}
+            >
+              <p>X</p>
+            </button>
+          </div>
         </div>
         <div className="browser-chats">
           {chats &&
             chats.map((chat) => (
-              <div key={chat._id}>
+              <div key={chat._id} className="chat-item">
+                <div className="chat-icon">
+                  <img
+                    src={chat.tipo === "publica" ? publicRoom : privateRoom}
+                    alt="ícone do chat"
+                  />
+                </div>
                 <p>{chat.nome}</p>
+                <Link to={`/chat/${chat._id}`}>Entrar</Link>
               </div>
             ))}
         </div>
